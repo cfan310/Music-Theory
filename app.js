44ab3a1c557a0790d3061413259ba57553be7070
cfan310 <cfanning43@gmail.com>
2023-06-27 10:10:05 -0700

    added variable for keyboard2

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // update html string / fret heirarchy
+
+// next steps: figure out how you want to structure the html/css to display the notes 
+// each note needs a unique id that displays as a light purple or green circle 
+// build out full fretboard
+// then use js to create function that displays the fretboard dymically
+
+let keyboard2 = document.getElementById("keyboard2");
